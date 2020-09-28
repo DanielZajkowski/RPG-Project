cbb11d507b78c849a6e208051092bb01c31b185e
DanielZajkowski <zajkowskidaniel@gmail.com>
2020-09-28 02:02:57 +0200

    adding weapon and projectiles

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,14 +9,23 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float speed = 1.0f;
+        [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
         Health target = null;
         float damage = 0.0f;
+
+        private void Start() 
+        {
+            transform.LookAt(GetAimLocation());
+        }
         
         void Update()
         {
             if (target == null) return;
-
-            transform.LookAt(GetAimLocation());
+            if(isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -39,7 +48,14 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other) 
         {
             if(other.GetComponent<Health>() != target) return;
+            if(target.IsDead()) return;
             target.TakeDamage(damage);
+
+            if(hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+            
             Destroy(gameObject);
         }
     }
