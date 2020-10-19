e26072347078740a21e58e1528774e1090d2fc5d
DanielZajkowski <zajkowskidaniel@gmail.com>
2020-10-19 12:53:48 +0200

    Character Stats

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 using RPG.Saving;
-using System.Collections.Generic;
+using RPG.Resources;
 
 namespace RPG.Movement
 {
--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -1,26 +1,39 @@
 using UnityEngine;
 using RPG.Saving;
+using RPG.Stats;
+using RPG.Core;
+using System;
 
-namespace RPG.Core
+namespace RPG.Resources
 {
     public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float healthPoints = 100f;
         bool isDead = false;
 
+        private void Start() 
+        {
+            healthPoints = GetComponent<BaseStats>().GetHealth();
+        }
+
         public bool IsDead()
         {
             return isDead;
         }
 
-        public void TakeDamage(float damage)
+        public void TakeDamage(GameObject instigator, float damage)
         {
             healthPoints = Mathf.Max(healthPoints - damage, 0);
-            // print(healthPoints);
             if (healthPoints == 0)
             {
                 Die();
+                AwardExperience(instigator);
             }
+        }
+
+        public float GetPercentage()
+        {
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetHealth());
         }
 
         private void Die()
@@ -30,6 +43,14 @@ namespace RPG.Core
             isDead = true;
             GetComponent<Animator>().SetTrigger("die");
             GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AwardExperience(GameObject instigator)
+        {
+            Experience experience = instigator.GetComponent<Experience>();
+            if(experience == null)  return;
+
+            experience.GainExperience(GetComponent<BaseStats>().GetExperienceReward());
         }
 
         public object CaptureState()
