65b0e1c9243a5516a6c2a61cefae15e5c90a187d
DanielZajkowski <zajkowskidaniel@gmail.com>
2020-11-17 19:06:27 +0100

    Update health UI

---
+++ Assets/Scripts/UI/DamageText/Destroyer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.UI.DamageText
+{
+    public class Destroyer : MonoBehaviour
+    {
+        [SerializeField] GameObject targetToDestroy = null;
+
+        public void DestroyTarget()
+        {
+            Destroy(targetToDestroy);
+        }
+    }
+}
